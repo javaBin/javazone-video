ff9c1af0e71ff5204911f90231d0b9fe63b7219d
Knut Haugen <knuthaug+github@gmail.com>
2011-10-15 20:47:39 +0200

    added small class for reading image from url

--- app/ImageFetcher.java
+++ app/ImageFetcher.java
@@ -1,6 +1,30 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * User: Knut Haugen <knuthaug@gmail.com>
  * 2011-10-15
  */
 public class ImageFetcher {
+
+
+    public static BufferedImage fetch(String url) {
+        if(url == null || url.equals("")) {
+            throw new IllegalArgumentException("invalid url given as argument to fetch(URL)");
+        }
+
+        try {
+            URL newURL = new URL(url);
+            return ImageIO.read(newURL);
+        } catch (MalformedURLException e) {
+            return new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+        } catch (IOException e) {
+            return new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+        }
+
+
+    }
 }
--- test/ImageFetcherTest.java
+++ test/ImageFetcherTest.java
@@ -1,6 +1,37 @@
+import org.junit.Test;
+import play.test.FunctionalTest;
+
+import java.awt.image.BufferedImage;
+
 /**
  * User: Knut Haugen <knuthaug@gmail.com>
  * 2011-10-15
  */
-public class ImageFetcherTest {
+public class ImageFetcherTest extends FunctionalTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void wontFetchNonvalidUrl() {
+        ImageFetcher.fetch("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void wontFetchNullUrl() {
+        ImageFetcher.fetch(null);
+    }
+
+    @Test
+    public void fetchesBufferedImageForValidUrl() {
+        String testUrl = "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0";
+        BufferedImage image = ImageFetcher.fetch(testUrl);
+        assertEquals(534, image.getWidth());
+    }
+
+    @Test
+    public void validButNonFunctionalUrlReturnsEmptyPicture() {
+        String testUrl = "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a210424-bb57-432a-bbfa-9766b90546d2/speaker-photos/0";
+        BufferedImage image = ImageFetcher.fetch(testUrl);
+        assertEquals(1, image.getWidth());
+    }
+
+
 }

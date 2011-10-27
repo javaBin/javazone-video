43b1ae3b2addb28aa8fe5777e42dbcb8e28cbc72
Knut Haugen <knuthaug+github@gmail.com>
2011-10-27 21:23:11 +0200

    sorts talks on frontpage by plays desc

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Application extends Controller {
 
     public static void index() {
-        List<Talk> talks = Talk.findAll();
+        List<Talk> talks = Talk.filter("year >", 1900).order("-plays").asList();
         render(talks);
     }
 

767cf2a455d168fb51a18869ef0c9dd1405778bd
Knut Haugen <knuthaug+github@gmail.com>
2011-11-03 19:54:31 +0100

    added talk filtering to tags

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.domain.Talk;
+import play.data.validation.Required;
 import play.mvc.Controller;
 
 import java.util.List;
@@ -10,6 +11,16 @@ public class Application extends Controller {
     public static void index() {
         List<Talk> talks = Talk.filter("year =", 2011).order("-plays").asList();
         render(talks);
+    }
+
+    public static void filter(@Required String tag) {
+        List<Talk> talks = Talk.filter("tags.name =", tag).order("-plays").asList();
+
+        if(talks == null) {
+            notFound("The talk was not found. Sorry");
+        }
+
+        renderTemplate("Application/index.html", talks);
     }
 
 }

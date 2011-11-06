b01dd49ba7e79b87af433854b1ca49617c1e16ce
Knut Haugen <knuthaug+github@gmail.com>
2011-11-06 21:06:27 +0100

    added comment

--- test/models/VideoFetcher.java
+++ test/models/VideoFetcher.java
@@ -11,8 +11,11 @@ import play.test.FunctionalTest;
 import java.util.List;
 
 /**
- * User: Knut Haugen <knuthaug@gmail.com>
- * 2011-09-24
+ * User: Knut Haugen <knuthaug@gmail.com> 2011-09-24
+ *
+ * This is a functional test written to easily fetch videos from video, session info from incogito and
+ * merge them and store in the database. The test should be ignore when not running it since
+ * running time is long.
  */
 public class VideoFetcher extends FunctionalTest {
 

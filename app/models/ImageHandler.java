86c5ca1702730ea61fe1df2def005544b94c15af
Knut Haugen <knuthaug+github@gmail.com>
2011-11-09 21:15:26 +0100

    added test and code for scaling two versions of the same image

--- app/models/ImageHandler.java
+++ app/models/ImageHandler.java
@@ -1,11 +1,10 @@
 package models;
 
-import models.ImageFetcher;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * User: Knut Haugen <knuthaug@gmail.com>
@@ -19,27 +18,37 @@ public class ImageHandler {
         dir = new File(basedir);
     }
 
-    public void handleImage(String basename, int year, String imageUrl) {
-        File file = getFileName(basename, year);
+    public void handleImage(String basename, int year, String imageUrl, Map<String, Integer> sizes) {
+
+        for(String sizeName : sizes.keySet()) {
+            handleFile(basename, year, imageUrl, sizeName, sizes.get(sizeName));
+        }
+
+    }
+
+    private void handleFile(String basename, int year, String imageUrl, String sizeName, int size) {
+        File file = getFileName(basename, year, sizeName);
+
         if(file.exists()) {
             return;
         }
 
-        File previousFile = getFileName(basename, year - 1);
+        File previousFile = getFileName(basename, year - 1, sizeName);
+
         if(previousFile.exists()) {
             previousFile.delete();
         }
 
-        BufferedImage image =  ImageResizer.resize(ImageFetcher.fetch(imageUrl));
+        BufferedImage image =  ImageResizer.resize(ImageFetcher.fetch(imageUrl), size);
+
         try {
             ImageIO.write(image, "jpeg", file);
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-
     }
 
-    private File getFileName(String basename, int year) {
-        return new File(dir + "/" + basename + "_" + year + ".jpg");
+    private File getFileName(String basename, int year, String size) {
+        return new File(dir + "/" + basename + "_" + year + "_" + size + ".jpg");
     }
 }
--- test/models/ImageHandlerTest.java
+++ test/models/ImageHandlerTest.java
@@ -1,7 +1,6 @@
 package models;
 
 import junit.framework.AssertionFailedError;
-import models.ImageHandler;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -11,6 +10,7 @@ import play.test.FunctionalTest;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.HashMap;
 
 /**
  * User: Knut Haugen <knuthaug@gmail.com>
@@ -30,47 +30,74 @@ public class ImageHandlerTest extends FunctionalTest {
 
     @After
     public void tearDown() {
-        new File("test/out/test_2011.jpg").delete();
+        new File("test/out/test_2011_large.jpg").delete();
+        new File("test/out/test_2011_small.jpg").delete();
     }
 
     @Test
     public void savesImageForNewAuthorToDisk() {
         ImageHandler handler = new ImageHandler("test/out");
         handler.handleImage("test", 2011,
-                "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0");
+                "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0",
+                new HashMap<String, Integer>(){{put("large", 300);}});
 
-        assertFileExist("test/out/test_2011.jpg");
+        assertFileExist("test/out/test_2011_large.jpg");
 
     }
 
     @Test
     public void doesNotOverwriteImageForSameAuthorAndYear() throws Exception {
-        File test = new File("test/out/test_2011.jpg");
+        File test = new File("test/out/test_2011_large.jpg");
         test.createNewFile();
 
         ImageHandler handler = new ImageHandler("test/out");
         handler.handleImage("test", 2011,
-                "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0");
+                "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0",
+                new HashMap<String, Integer>(){{put("large", 300);}});
 
-        assertFileExist("test/out/test_2011.jpg");
+        assertFileExist("test/out/test_2011_large.jpg");
         assertEquals(null, ImageIO.read(test));
     }
 
      @Test
     public void updatesImagesForMoreRecentYear() throws Exception {
-         File test = new File("test/out/test_2010.jpg");
+         File test = new File("test/out/test_2010_large.jpg");
          test.createNewFile();
 
          ImageHandler handler = new ImageHandler("test/out");
 
          handler.handleImage("test", 2011,
-                 "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0");
+                 "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0",
+                 new HashMap<String, Integer>(){{put("large", 300);}});
 
-         assertFileExist("test/out/test_2011.jpg");
-         assertFileDoesntExist("test/out/test_2010.jpg");
-         BufferedImage in = ImageIO.read(new File("test/out/test_2011.jpg"));
+         assertFileExist("test/out/test_2011_large.jpg");
+         assertFileDoesntExist("test/out/test_2010_large.jpg");
+         BufferedImage in = ImageIO.read(new File("test/out/test_2011_large.jpg"));
          assertEquals(200, in.getWidth());  //scaled
     }
+
+
+    @Test
+    public void savesSmallImageVersionForNewAuthorToDisk() {
+        ImageHandler handler = new ImageHandler("test/out");
+        handler.handleImage("test", 2011,
+                "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0",
+                new HashMap<String, Integer>(){{put("large", 300); put("small", 100);}});
+
+        assertFileExist("test/out/test_2011_small.jpg");
+    }
+
+    @Test
+    public void smallImageIsScaledAccordingly() throws Exception {
+        ImageHandler handler = new ImageHandler("test/out");
+        handler.handleImage("test", 2011,
+                "http://javazone.no/incogito10/rest/events/JavaZone%202011/sessions/a21042d4-bb57-432a-bbfa-9766b90546d2/speaker-photos/0",
+                new HashMap<String, Integer>(){{put("large", 300); put("small", 100);}});
+
+        BufferedImage in = ImageIO.read(new File("test/out/test_2011_small.jpg"));
+        assertEquals(100, in.getHeight());  //scaled
+    }
+
 
     private void assertFileDoesntExist(String file) {
        File test = new File(file);

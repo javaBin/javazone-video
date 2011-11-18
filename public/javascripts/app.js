28982d9ba210a5fc0753817b3f5c12c9f88864a3
Bjarte Stien Karlsen <bjarte@bjartek.org>
2011-11-18 23:31:12 +0100

    moved tags to top. implemented tag search in text search

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,35 +1,47 @@
-$(function() {
+$(function () {
 
 
-    var filterArticles = function(filters) {
-        $('article').each(function() {
+    var filterArticles = function (filters) {
+        $('article').each(function () {
             var article = $(this);
-            var tags = article.find(".tag").map(function(){
-                        return this.text.toLowerCase().replace(" ", "_");
-                    });
+            var tags = article.find(".tag").map(function () {
+                return this.text.toLowerCase().replace(" ", "_");
+            });
             var visible = article.is(":visible");
             var valid = _.difference(filters, tags).length == 0;
 
-            if(valid && !visible) {
+            if (valid && !visible) {
                 article.fadeIn()
             }
 
-            if(!valid && visible) {
+            if (!valid && visible) {
                 article.fadeOut();
             }
         });
 
     }
 
-    $("#filter-tags" ).buttonset();
+    $("#filter-tags").buttonset();
 
-    $("#filter-tags label").click(function(event){
-        var filters = $(".ui-state-active").map(function() {
+    $("#filter-tags label").click(function (event) {
+        var filters = $(".ui-state-active").map(function () {
             return $(this).attr("for").toLowerCase();
         });
         filterArticles(filters);
 
-   });
+
+    });
+
+    $("#filter-form").submit(function () {
+
+        var input = $("#filter").val();
+        if (input == "") {
+            filterArticles([]);
+        } else {
+            filterArticles(input.split(" "));
+        }
+        return false;
+    });
 
 
- });
+});

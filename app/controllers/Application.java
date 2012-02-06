16c84fef5d2e952411292eabc6a31225e8572f9f
Knut Haugen <knuthaug+github@gmail.com>
2012-02-06 21:14:47 +0100

    Added talks to speaker index page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,11 +12,13 @@ import static models.GuavaTools.collect;
 
 public class Application extends Controller {
 
+    static List<Integer> years = Lists.newArrayList(2010, 2011);
+
     public static void index() {
         List<Talk> talks = Talk.filter("year =", 2011).order("-plays").asList();
         Iterable<String> allTags = collect(talks, Talk.findTags());
         List<String> tags = GuavaTools.findMostPopularElements(allTags, 20);
-        List<Integer> years = Lists.newArrayList(2010, 2011);
+
         render(talks, tags, years);
     }
 
@@ -26,7 +28,6 @@ public class Application extends Controller {
 
         Iterable<String> allTags = collect(talks, Talk.findTags());
         List<String> tags = GuavaTools.findMostPopularElements(allTags, 10);
-        List<Integer> years = Lists.newArrayList(2010, 2011);
 
         if(talks == null) {
             notFound("No talks found for the current query. Sorry");
--- app/controllers/Speakers.java
+++ app/controllers/Speakers.java
@@ -5,6 +5,9 @@ import models.domain.Talk;
 import play.data.validation.Required;
 import play.mvc.Controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -26,6 +29,36 @@ public class Speakers extends Controller {
 
     public static void index() {
         List<Speaker> speakers = Speaker.all().asList();
-        render(speakers);
+        List<Talk> talkList = Talk.all().asList();
+        HashMap talks = findTalksForSpeakers(talkList);
+        render(speakers, talks);
+    }
+
+    private static HashMap findTalksForSpeakers(List<Talk> talkList) {
+        HashMap talks = new LinkedHashMap<String, List<Talk>>();
+
+        for(Talk talk : talkList) {
+            addTalkToSpeakerList(talks, talk);
+        }
+        return talks;
+    }
+
+    private static void addTalkToSpeakerList(HashMap talks, Talk talk) {
+        List<Speaker> talkSpeakers = talk.speakers();
+
+        for(Speaker speaker : talkSpeakers) {
+            checkForTalkAddition(talks, talk, speaker);
+        }
+    }
+
+    private static void checkForTalkAddition(HashMap talks, Talk talk, Speaker speaker) {
+        if(talks.containsKey(speaker.slug())) {
+            List<Talk> speakerTalks = (List<Talk>) talks.get(speaker.slug());
+            speakerTalks.add(talk);
+        } else {
+            List<Talk> addedTalks = new ArrayList<Talk>();
+            addedTalks.add(talk);
+            talks.put(speaker.slug(), addedTalks);
+        }
     }
 }

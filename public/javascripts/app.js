01d3ebfcf735a1b36e834419a68cd6681242108a
Knut Haugen <knuthaug+github@gmail.com>
2012-08-21 18:55:37 +0200

    scrollbar and all tweets for widget

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -149,10 +149,10 @@ jz.common_props = {
         }
     },
     features: {
-        scrollbar: false,
+        scrollbar: true,
         loop: true,
         live: false,
-        behavior: 'default'
+        behavior: 'all'
     }  
 };
 
